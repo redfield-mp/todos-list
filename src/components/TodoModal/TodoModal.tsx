83b7545a0335a14f4219db2639bbd20aa4bf7253
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-06-30 19:11:02 +0300

    add TodoModal component with user details

---
+++ src/components/TodoModal/TodoModal.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { currentTodoSlice } from '../../features/currentTodo';
+
+export const TodoModal: React.FC = () => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const dispatch = useAppDispatch();
+  const { setTodo } = currentTodoSlice.actions;
+
+  useEffect(() => {
+    if (currentTodo) {
+      getUser(currentTodo.userId).then(setCurrentUser);
+    }
+  }, [currentTodo]);
+
+  return (
+    <>
+      {' '}
+      {currentTodo && (
+        <div className="modal is-active" data-cy="modal">
+          <div className="modal-background" />
+
+          {currentUser ? (
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <div
+                  className="modal-card-title has-text-weight-medium"
+                  data-cy="modal-header"
+                >
+                  Todo #{currentTodo.id}
+                </div>
+
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  type="button"
+                  className="delete"
+                  data-cy="modal-close"
+                  onClick={() => dispatch(setTodo(null))}
+                />
+              </header>
+
+              <div className="modal-card-body">
+                <p className="block" data-cy="modal-title">
+                  {currentTodo.title}
+                </p>
+
+                <p className="block" data-cy="modal-user">
+                  {currentTodo.completed ? (
+                    <strong className="has-text-success">Done</strong>
+                  ) : (
+                    <strong className="has-text-danger">Planned</strong>
+                  )}
+
+                  {' by '}
+                  <a href={`mailto:${currentUser?.email}`}>
+                    {currentUser.name}
+                  </a>
+                </p>
+              </div>
+            </div>
+          ) : (
+            <Loader />
+          )}
+        </div>
+      )}{' '}
+    </>
+  );
+};
